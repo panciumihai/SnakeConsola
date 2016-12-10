d731ba81f16c7dd4a150a44cd17b6bd1b4819dac
Panciu-Rusu Mihai <panciu.rusu.mihai@gmail.com>
2016-12-10 20:55:29 +0200

    Creare harta
    Crearea conceptului de harta a jocului.

--- Panciu-Rusu Mihai Snake/Panciu-Rusu Mihai Snake/Source.cpp
+++ Panciu-Rusu Mihai Snake/Panciu-Rusu Mihai Snake/Source.cpp
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+
 using namespace std;
+
+#define LATIME_HARTA 25
+#define LUNGIME_HARTA 25
+
+int harta[LUNGIME_HARTA][LATIME_HARTA];
+
+void initializare_harta()
+{
+	for (int i = 0; i < LUNGIME_HARTA; ++i)
+		for (int j = 0; j < LATIME_HARTA; ++j)
+			harta[i][j] = 0;
+}
+
+void afisare_harta()
+{
+	for (int i = 0; i < LUNGIME_HARTA; ++i)
+	{
+		for (int j = 0; j < LATIME_HARTA; ++j)
+		{
+			switch (harta[i][j])
+			{
+			case 0:printf(" "); break;
+			default:printf("");
+				break;
+			}
+		}
+		printf("\n");
+	}
+}
 
 void main()
 {
-	printf("primul test\n");
+	initializare_harta();
+	afisare_harta();
 }
